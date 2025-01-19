73c06d17251d26289cd192dfcdd9e685a0edf7c1
Ansh Garhewal <me@anshdevs.in>
2025-01-19 13:26:57 +0530

    support for volume mounts via args

--- scripts/on_docker.sh
+++ scripts/on_docker.sh
@@ -7,12 +7,31 @@ else
   echo "Environment variable KM_API_KEY is set to: $KM_API_KEY"
 fi
 
+VOLUME_MOUNTS=()
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --volume|-v)
+      VOLUME_MOUNTS+=("$2")
+      shift 2
+      ;;
+    *)
+      echo "Unknown : $1"
+      exit 1
+      ;;
+  esac
+done
+
+VOLUME_ARGS=""
+for VOLUME in "${VOLUME_MOUNTS[@]}"; do
+  VOLUME_ARGS="$VOLUME_ARGS -v $VOLUME"
+done
+
+echo $VOLUME_ARGS
+
 docker run -d\
   --name kmagent \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
-  -v /proc:/hostfs/proc:ro \
-  -v /sys:/hostfs/sys:ro \
-  -v /:/hostfs:ro \
+  $VOLUME_ARGS \
   --privileged \
   --pid=host \
   ghcr.io/kloudmate/km-agent:latest \
