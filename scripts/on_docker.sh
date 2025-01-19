10a92ddca44c191af4bddc007f28ba1ecd08c9a4
Ansh Garhewal <me@anshdevs.in>
2025-01-19 12:26:39 +0530

    feat: added docker installation script

---
+++ scripts/on_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z "${KM_API_KEY}" ]; then
+  echo "Environment variable KM_API_KEY is not set."
+  exit 1
+else
+  echo "Environment variable KM_API_KEY is set to: $KM_API_KEY"
+fi
+
+docker run \
+  --name kmagent \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  -v /proc:/hostfs/proc:ro \
+  -v /sys:/hostfs/sys:ro \
+  -v /:/hostfs:ro \
+  --privileged \
+  --pid=host \
+  kmagent:latest \
+  /kmagent -m=docker -t=${KM_API_KEY} start
