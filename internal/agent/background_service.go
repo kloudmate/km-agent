c73fe57f30115d92180f8de8d009ad4f86c192ba
Ansh Garhewal <me@anshdevs.in>
2025-04-15 10:21:20 +0530

    init collector config on each startup

--- internal/agent/background_service.go
+++ internal/agent/background_service.go
@@ -95,8 +95,13 @@ func (svc *KmAgentService) asyncWork() {
 	}
 }
 
-func (svc *KmAgentService) Start(s bgsvc.Service) error {
+func (svc *KmAgentService) Start(s bgsvc.Service) (err error) {
 	svc.Svclogger.Infof("Running agent on %s mode \n", svc.Mode)
+	svc.ApplyAgentConfig(cli.NewContext(nil, nil, nil))
+	svc.Collector, err = collector.NewKmCollector(svc.Configs)
+	if err != nil {
+		return err
+	}
 	go svc.asyncWork()
 	return nil
 }
